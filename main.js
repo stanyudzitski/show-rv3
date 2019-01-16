82b7265245a4ff2b408beed66c45640812b53df3
StanDman <stas.yudzitski@yandex.ru>
2019-01-16 13:17:01 +0300

    another variable added

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ btn.addEventListener('click', function() {
     console.log('Btn clicked!');
 });
 
-let a = '333';
+const a = '333';
 console.log(a);
+
+const b = 'just a string';
+console.log(`this is a: ${a}, and this is b: ${b}`);
